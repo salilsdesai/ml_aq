b9c27bafbccb73e90dcdf26fca2a6714796cf203
Salil Desai <salilsdesai@gmail.com>
2020-11-25 14:21:05 -0500

    convert csv files to dbf to be loaded more easily

--- convert_data_file.py
+++ convert_data_file.py
@@ -17,15 +17,33 @@ def dbf_to_csv(filename):
         out_file.write(curr_line)
     out_file.close()
 
-def csv_to_dbf(filename):
+def csv_to_dbf(filename, field_name_convert=None):
     in_csv = open('data/' + filename + '.csv', 'r')
     headers = in_csv.readline().strip('\n\r').split(',')
     field_specs = ''
     for header in headers:
-        field_specs += ('; ' if len(field_specs) > 0 else '') + header + ' C(30)'
+        field_specs += ('; ' if len(field_specs) > 0 else '') + (field_name_convert(header) if field_name_convert is not None else header) + ' C(30)'
     out_file = dbf.Table('data/' + filename + '.dbf', field_specs)
     out_file.open(mode=dbf.READ_WRITE)
     for line in in_csv:
         vals = line.strip('\n\r').split(',')
         out_file.append(tuple(vals))
     out_file.close()
+
+csv_to_dbf('Emission Rates', lambda name: 'index' if name == '""' else (name.replace('emission', 'em').replace('"', '').replace('(','_').replace(')', '_')))
+csv_to_dbf('Met_1_1')
+repl = {
+    'DirectionID': 'direc_id',
+    'HourlyCapacity': 'hourly_cap',
+    'MOVESRoadTypeID': 'mv_rd_t_id',
+    'FacilityTypeID': 'fac_typ_id',
+    'LinkTravelTime': 'lnk_tr_tim',
+    'IntersectionTravelTimen': 'int_tr_t_n',
+    'IntersectionTravelTimeo': 'int_tr_t_o',
+    'IncidentDelayTimeOriginal': 'inc_d_t_o',
+    'IncidentDelayTimeITS': 'inc_d_t_i',
+    'TotalTravelTime': 'tot_tr_tim',
+    'DetectionManagementCode': 'det_man_cd',
+    'QueuingFactor': 'queue_fac',
+}
+csv_to_dbf('Base File', lambda name: repl[name] if name in repl else name)
