0691b0df7f1d98627c934d0aeaf1a228725e62ed
Salil Desai <salilsdesai@gmail.com>
2020-12-09 18:13:23 -0500

    removed default values from met data averages properlyy

--- data/gather_data.py
+++ data/gather_data.py
@@ -1,45 +1,25 @@
+import pandas as pd
+
 SNAKE_CASE = lambda s: s.lower().replace(' ', '_')
+MET_STATIONS = ['NYC', 'LGA', 'JFK']
+MET_DATA_FIELDS = [
+	'Year', 'Month', 'Day', 'Julian Day', 'Hour', 'Measurement Height',
+	'Indicator Flag', 'Wind Direction', 'Wind Speed', 'Temperature',
+	'Lateral Wind Direction Standard Deviation',
+	'Vertical Wind Speed Standard Deviation', 'Sensible Heat Flux',
+	'Surface Friction Velocity', 'Convective Velocity Scale',
+	'Potential Temperature Gradient Above the Mixing Height',
+	'Convectively Driven Mixing Height', 'Mechanically Driven Mixing Height',
+	'Monin Obukhov Length', 'Surface Roughness Length', 'Bowen Ratio',
+	'Albedo', 'Wind Speed Stage 3', 'Wind Direction Stage 3',
+	'Anemometer Height Stage 3', 'Temperature Stage 3', 'Measurement Height Stage 3',
+	'Precipitation Type', 'Precipitation Amount', 'Relative Humidity',
+	'Station Pressure', 'Cloud Cover', 'Wind Speed Adjustment and Data Source Flag',
+	'Cloud Cover and Temperature Substition by Interpolation',
+]
 
 def gather_individual_met_data():
-	fields = [
-		'Year',
-		'Month',
-		'Day',
-		'Julian Day',
-		'Hour',
-		'Measurement Height',
-		'Indicator Flag',
-		'Wind Direction',
-		'Wind Speed',
-		'Temperature',
-		'Lateral Wind Direction Standard Deviation',
-		'Vertical Wind Speed Standard Deviation',
-		'Sensible Heat Flux',
-		'Surface Frication Velocity',
-		'Convective Velocity Scale',
-		'Potential Temperature Gradient Above the Mixing Height',
-		'Convectively Driven Mixing Height',
-		'Mechanically Driven Mixing Height',
-		'Monin Obukhov Length',
-		'Surface Roughness Length',
-		'Bowen Ratio',
-		'Albedo',
-		'Wind Speed Stage 3',
-		'Wind Direction Stage 3',
-		'Anemometer Height Stage 3',
-		'Temperature Stage 3',
-		'Measurement Height Stage 3',
-		'Precipitation Type',
-		'Precipitation Amount',
-		'Relative Humidity',
-		'Station Pressure',
-		'Cloud Cover',
-		'Wind Speed Adjustment and Data Source Flag',
-		'Cloud Cover and Temperature Substition by Interpolation',
-	]
-	
-	for station_name in ['JFK', 'LGA', 'NYC']:
-		
+	for station_name in MET_STATIONS:
 		pfl_file = open('data/ML_AQ/met/' + station_name + '-1317.PFL.PFL', 'r')
 		sfc_file = open('data/ML_AQ/met/' + station_name + '-1317.SFC.SFC', 'r')
 		pfl = iter(pfl_file)
@@ -48,9 +28,9 @@ def gather_individual_met_data():
 
 		out_file = open('data/' + station_name.lower() + '_met_data.csv', 'w')
 
-		out_file.write(SNAKE_CASE(fields[0]))
-		for i in range(1, len(fields)):
-			out_file.write(',' + SNAKE_CASE(fields[i]))
+		out_file.write(SNAKE_CASE(MET_DATA_FIELDS[0]))
+		for i in range(1, len(MET_DATA_FIELDS)):
+			out_file.write(',' + SNAKE_CASE(MET_DATA_FIELDS[i]))
 
 		stop_iteration = False
 		while not stop_iteration:
@@ -70,63 +50,26 @@ def gather_individual_met_data():
 		sfc_file.close()
 		out_file.close()
 
+
 def gather_average_met_data():
-	fields = [
-		None,
-		None,
-		None,
-		None,
-		None,
-		None,
-		'Wind Direction',
-		'Wind Speed',
-		'Temperature',
-		None,
-		None,
-		None,
-		None,
-		None,
-		None,
-		None,
-		'Sensible Heat Flux',
-		'Surface Frication Velocity',
-		'Convective Velocity Scale',
-		'Potential Temperature Gradient Above the Mixing Height',
-		'Convectively Driven Mixing Height',
-		'Mechanically Driven Mixing Height',
-		'Monin Obukhov Length',
-		'Surface Roughness Length',
-		'Bowen Ratio',
-		'Albedo',
-		'Wind Speed Stage 3',
-		'Wind Direction Stage 3',
-		None,
-		'Temperature Stage 3',
-		None,
-		None,
-		'Precipitation Amount',
-		'Relative Humidity',
-		'Station Pressure',
-		'Cloud Cover',
-		None,
-		None,
+	default_values = [
+		None, None, None, None, None, None, None, 999, 999, 99.9, None, None, 
+		-999, -9, -9, -9, -999, -999, -99999, -9, -9, -9, 999, 999,
+		None, 999, None, 9999, -9, 999, 99999, 99, None, None
 	]
 
-	empty_values = set([99999, 9999, 999, 99, 99.9, 9, -9, -99, -999, -9999, -99999])
-	stations = [(2, 'NYC'), (3, 'LGA'), (4, 'JFK')]
 	out_file = open('data/met_data.csv', 'w')
 
 	out_file.write('id,name,latitude,longitude')
-	for f in fields:
-		if f is not None:
-			out_file.write(',' + f)
+	for i in range(len(MET_DATA_FIELDS)):
+		if default_values[i] is not None:
+			out_file.write(',' + MET_DATA_FIELDS[i])
 
-	for index in range(len(stations)):
-
-		station_id, station_name = stations[index]
-
-		values_sums = [0] * len(fields)
-		values_counts = [0] * len(fields)
+	for index in range(len(MET_STATIONS)):
+		station_name = MET_STATIONS[index]
+		station_id = index + 2
+		values_sums = [0] * len(MET_DATA_FIELDS)
+		values_counts = [0] * len(MET_DATA_FIELDS)
 		
 		pfl_file = open('data/ML_AQ/met/' + station_name + '-1317.PFL.PFL', 'r')
 		sfc_file = open('data/ML_AQ/met/' + station_name + '-1317.SFC.SFC', 'r')
@@ -142,10 +85,9 @@ def gather_average_met_data():
 			try:
 				pfl_items = next(pfl).strip('\n\r').split()
 				sfc_items = next(sfc).strip('\n\r').split()
-				vals = pfl_items + sfc_items
-
+				vals = sfc_items[0:5] + pfl_items[4:] + sfc_items[5:]
 				for i in range(len(vals)):
-					if fields[i] is not None and ((fields[i] == 'Cloud Cover') or (float(vals[i]) not in empty_values)):
+					if default_values[i] is not None and (float(vals[i]) != default_values[i]):
 						values_sums[i] += float(vals[i])
 						values_counts[i] += 1
 			except StopIteration:
@@ -155,14 +97,13 @@ def gather_average_met_data():
 		sfc_file.close()
 
 		out_file.write('\n' + str(station_id) + ',' + station_name + ',' + latitude + ',' + longitude)
-		for i in range(len(fields)):
-			if fields[i] is not None:
+		for i in range(len(MET_DATA_FIELDS)):
+			if default_values[i] is not None:
 				out_file.write(',' + str(values_sums[i]/values_counts[i]))
-
 	out_file.close()
+
+
 
 if __name__ == "__main__":
 	gather_individual_met_data()
 	gather_average_met_data()
-
-	
