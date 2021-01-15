9b3b7a9cb7fa42a290bc2212ad6b0bcce181f3b1
Salil Desai <salilsdesai@gmail.com>
2021-01-14 21:33:35 -0500

    added wind direction and speed to model

--- model.py
+++ model.py
@@ -14,42 +14,13 @@ ERROR_FUNCTION = torch.nn.MSELoss()
 BATCH_SIZE = 1000
 
 # Used to load means and std devs for feature normilization
+# Must be in the same order as the features in the tensor passed into the model
 FEATURES = [
 	'distance', 'elevation_difference', 'vmt', 'traffic_speed', 
 	'fleet_mix_light', 'fleet_mix_medium', 'fleet_mix_heavy', 
-	'fleet_mix_commercial', 'fleet_mix_bus',
+	'fleet_mix_commercial', 'fleet_mix_bus', 'wind_direction', 'wind_speed',
 ]
 INPUT_SIZE = len(FEATURES)
-
-def extract_list(filepath, class_name=''):
-	df = pd.read_csv(filepath)
-	return [type(class_name, (object,), dict(row[1])) for row in df.iterrows()]
-
-def prep_links(links=None):
-	if links is None:
-		links = extract_list('data/link_data.csv', 'link')
-	coords = torch.DoubleTensor([[l.x, l.y] for l in links]).T.to(DEVICE)
-	coords_dot = (coords * coords).sum(dim=0)
-	subtract = torch.Tensor([[l.elevation_mean] for l in links]).to(DEVICE)
-	keep = torch.Tensor([[[
-	  link.traffic_flow * link.link_length,
-	  link.traffic_speed,
-	  link.fleet_mix_light,
-	  link.fleet_mix_medium,
-	  link.fleet_mix_heavy,
-	  link.fleet_mix_commercial,
-	  link.fleet_mix_bus
-	] for link in links]]).to(DEVICE).repeat(BATCH_SIZE, 1, 1)
-	# If too much memory or need multiple batch sizes, move repeat to make_x (distances.shape[0])
-	return (coords, coords_dot, subtract, keep)
-  
-def prep_receptors(receptors=None):
-	if receptors is None:
-		receptors = extract_list('data/receptor_data.csv', 'receptor')
-	coords = torch.DoubleTensor([[r.x, r.y] for r in receptors]).to(DEVICE)
-	coords_dot = (coords * coords).sum(dim=1).unsqueeze(dim=-1)
-	subtract = torch.Tensor([[[r.elevation]] for r in receptors]).to(DEVICE)
-	return (coords, coords_dot, subtract)
 
 def load_feature_stats(filepath):
 	"""
@@ -69,6 +40,39 @@ def load_feature_stats(filepath):
 	return feature_stats.to(DEVICE)
 
 FEATURE_STATS = load_feature_stats('data/feature_stats.txt')
+MET_DATA = {(int(row[1]['id'])):(type('MetStation', (object,), dict(row[1]))) for row in pd.read_csv('data/met_data.csv').iterrows()}
+
+def extract_list(filepath, class_name=''):
+	df = pd.read_csv(filepath)
+	return [type(class_name, (object,), dict(row[1])) for row in df.iterrows()]
+
+def prep_links(links=None):
+	if links is None:
+		links = extract_list('data/link_data.csv', 'link')
+	coords = torch.DoubleTensor([[l.x, l.y] for l in links]).T.to(DEVICE)
+	coords_dot = (coords * coords).sum(dim=0)
+	subtract = torch.Tensor([[l.elevation_mean] for l in links]).to(DEVICE)
+	keep = torch.Tensor([[[
+		link.traffic_flow * link.link_length,
+		link.traffic_speed,
+		link.fleet_mix_light,
+		link.fleet_mix_medium,
+		link.fleet_mix_heavy,
+		link.fleet_mix_commercial,
+		link.fleet_mix_bus,
+		MET_DATA[link.nearest_met_station_id].wind_direction,
+		MET_DATA[link.nearest_met_station_id].wind_speed,
+	] for link in links]]).to(DEVICE).repeat(BATCH_SIZE, 1, 1)
+	# If too much memory or need multiple batch sizes, move repeat to make_x (distances.shape[0])
+	return (coords, coords_dot, subtract, keep)
+  
+def prep_receptors(receptors=None):
+	if receptors is None:
+		receptors = extract_list('data/receptor_data.csv', 'receptor')
+	coords = torch.DoubleTensor([[r.x, r.y] for r in receptors]).to(DEVICE)
+	coords_dot = (coords * coords).sum(dim=1).unsqueeze(dim=-1)
+	subtract = torch.Tensor([[[r.elevation]] for r in receptors]).to(DEVICE)
+	return (coords, coords_dot, subtract)
 
 class Model(torch.nn.Module):
 	def __init__(self):
